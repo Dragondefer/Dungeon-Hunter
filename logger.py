038044153bc70a3017563c28515e79b8f8033d7c
Dragondefer <119514487+Dragondefer@users.noreply.github.com>
2025-05-12 19:06:40 +0200

    Added logger.py
    This file allow to log the player's action into a log file so it will help a LOT the devs. thanks for your undestanding. (note; it isn't a keylogger, as it doens't register all the key pressed but only a comment. e.g. : logger.warning("this is a warning")

---
+++ logger.py
@@ -0,0 +1,19 @@
+import logging
+import os
+from datetime import datetime
+
+# Dossier des logs
+if not os.path.exists("logs"):
+    os.makedirs("logs")
+
+# Nom unique basé sur la date/heure
+log_filename = datetime.now().strftime("logs/session_%Y-%m-%d_%H-%M-%S.log")
+
+# Configuration du logger
+logging.basicConfig(
+    filename=log_filename,
+    level=logging.DEBUG,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+logger = logging.getLogger()
