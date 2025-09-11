1dc217dfad9f0fe4e8e7d14598f3ff1b5734f42a
Dragondefer <119514487+Dragondefer@users.noreply.github.com>
2025-09-11 22:46:56 +0200

    Edited spells location

--- items/__init__.py
+++ items/__init__.py
@@ -1,3 +1,6 @@
+# Dungeon Hunter - (c) Dragondefer 2025
+# Licensed under CC BY-NC 4.0
+
 from sys import path
 from os.path import abspath, dirname, join
 # Define the root of the project
@@ -6,4 +9,5 @@ path.append(abspath(join(dirname(__file__), '..')))
 
 from .inventory import Inventory
 from .items import Item, Gear, Weapon, Armor, Shield, Gauntlets, Amulet, Ring, Belt, Potion, Equipment
-from .spells import Spell, Scroll
+from core.spells import Spell, Scroll
+
