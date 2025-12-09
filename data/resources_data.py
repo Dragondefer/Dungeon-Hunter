8f0df0730d6853774cc98d517cff693b67864ba7
Dragondefer <119514487+Dragondefer@users.noreply.github.com>
2025-12-09 13:58:30 +0100

    Ressouce update
    Added ResourceType class with enum for better handling for devs (for me)

--- data/resources_data.py
+++ data/resources_data.py
@@ -1,9 +1,20 @@
 # Dungeon Hunter - (c) Dragondefer 2025
 # Licensed under CC BY-NC 4.0
 
+from enum import Enum, auto
+
+class ResourceType(Enum):
+    ORE = auto()
+    PLANT = auto()
+    MAGIC = auto()
+    FOOD = auto()
+
+
+
 resources_data_raw = {
     "iron_ore": {
         "name": "Iron Ore",
+        "type": ResourceType.ORE,
         "rarity": "common",
         "description": "A basic metal used in forging.",
         "value": 5
@@ -11,18 +22,21 @@ resources_data_raw = {
     "magic_essence": {
         "name": "Magic Essence",
         "rarity": "rare",
+        "type": ResourceType.MAGIC,
         "description": "A glowing crystal full of arcane power.",
         "value": 50
     },
     "smithing_stone": {
         "name": "Smithing Stone",
         "rarity": "common",
+        "type": ResourceType.ORE,
         "description": "A stone used to upgrade weapons.",
         "value": 10
     },
     "wood": {
         "name": "Wood",
         "rarity": "common",
+        "type": ResourceType.PLANT,
         "description": "Basic wood used for crafting.",
         "value": 3
     }
