afa3fa38801d835431c6f8bface2e4bd42f3e6d4
Dragondefer <119514487+Dragondefer@users.noreply.github.com>
2026-03-08 18:28:34 +0100

    internal core update
    added new function to setup the ia

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-import os
+from os.path import exists
 
 # Global game speed setting and multipliers
 game_speed = "normal"
@@ -21,13 +21,43 @@ def set_game_speed_multiplier(multiplier: float):
 
 try:
     DEV_AGENT_MODE = False
-    if os.path.exists("ai/agent_wrapper.py"):
+    if exists("ai/agent_wrapper.py"):
         from ai.agent_wrapper import get_agent
         agent_instance = get_agent()
         DEV_AGENT_MODE = True
 except Exception as e:
     agent_instance = None
     DEV_AGENT_MODE = False
+
+
+# AI dev integration
+import os
+
+agent_is_enabled = lambda *args, **kwargs: None
+get_agent = lambda *args, **kwargs: None
+aw = None
+
+def setup_ai(choice):
+    global agent_is_enabled, get_agent, aw
+    try:
+        if os.path.exists("./ai/agent.py") and os.path.exists("./ai/agent_wrapper.py"):
+            try:
+                from ai.agent_wrapper import agent_is_enabled as real_agent_is_enabled
+                import ai.agent_wrapper as aw_module
+                from ai.agent_wrapper import get_agent as real_get_agent
+                agent_is_enabled = real_agent_is_enabled
+                get_agent = real_get_agent
+                aw = aw_module
+            except Exception as e:
+                print("Cannot import agent_wrapper")
+            if choice == "y" and aw:
+                aw.enable_agent()
+            else:
+                if aw: aw.disable_agent()
+        else:
+            pass
+    except Exception as e:
+        pass
 
 
 # Developer mode flag
